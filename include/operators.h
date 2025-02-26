a674381a37596abf93703ed79b13e9e50452325b
Jack Brown <jackcbrown89@gmail.com>
2025-02-25 23:43:48 -0800

    Updates

--- include/operators.h
+++ include/operators.h
@@ -4,11 +4,43 @@ typedef struct {
     float mult_factor;
     float add_factor; 
     int offset; 
-    int length; 
+    int length;
+    bool bit_mode;
 } Message_operator; 
 
-const int NUM_MESSAGE_OPERATORS = 1;
+/**
+ * "Bit mode" example
+ * [0x2F, 0xF5, 0xA3] = [00101111, 11110101, 10110011] -> little endian = [10110011, 11110101, 00101111]
+ * Offset: 12 -> [10110011, 1111~0~101, 00101111]
+ * Length: 8 -> [0010~1111~, 1111~0101, 1011~0011]
+ * -> 010110110011
+ */
+
+
+const int NUM_MESSAGE_OPERATORS = 2;
 const Message_operator message_operators[] = {
     // define your operators here
-    {"RPM", 0x0A5, 0.25, 0, 5, 2},
+    // {"RPM", 0x0A5, 0.25, 0, 5, 2, false},
+    {"Engine Torque", 0x0A5, 0.5, -1023.5, 16, 12, true},
+    // {"Steering1", 0x301, -1/22.75, 1440, 2, 2, false},
+    // {"Steering2", 0x302, -1/22.75, 1440, 2, 2, false},
+    // {"Brake %", 0x0EF, -2, 250, 3, 1, false},
+    // {"Accelerator pedal %", 0x0D9, (1 / 40.0), 0, 16, 12, true},
+    // {"Fuel Range km", 0x330, 1 / 16.0, 0, 48, 12, true},
+    // {"Gear", 0x0F3, 1, -4, 16, 4, true},
+    {"Speed mph", 0x1A1, 0.015625 / 1.60934, 0, 2, 2, false}
 };
+
+/**
+ * Acceptance Mask
+ * 0x0A5, 0x302, 0x0EF, 0x0D9
+ * 
+ * 00010100101
+ * 01100000010
+ * 00011101111
+ * 00011011001
+ * 
+ *=01111111111 
+ *=0x3FF
+ * 
+ */
--- main/main.c
+++ main/main.c
@@ -1,3 +1,4 @@
+#include <math.h> 
 #include <stdio.h>
 
 #include "driver/gpio.h"
@@ -6,85 +7,99 @@
 #include "main.h"
 #include "operators.h" 
 
-void process_rpm_message(twai_message_t *msg) {
-    int msg_id = msg->identifier;
-    if (!msg->rtr) {
-        printf("%d\n", msg_id);
-        for (int i = 0; i < msg->data_length_code; i++) {
-            printf("[%x]", msg->data[i]);
-        }
-        printf("\n");
-    }
-}
 
 void process_message(twai_message_t *msg) {
+    // no need to process remote request frames
+    if (msg->rtr) {
+        return;
+    }
+
     int msg_id = msg->identifier;
+    uint8_t data_length = msg->data_length_code;
 
     for (int i = 0 ; i < NUM_MESSAGE_OPERATORS; i++) {
         if(msg_id == message_operators[i].message_id) {
             float mult_factor = message_operators[i].mult_factor;
             float add_factor = message_operators[i].add_factor;
-            int payload_start = message_operators[i].offset;
-            int payload_end = payload_start + message_operators[i].length;
 
-            int value = 0;
-            uint8_t shift = 0;
-            for (int j = payload_start; j < payload_end; j++) {
-                uint8_t datapoint = msg->data[j];
-                unsigned int shifted_datapoint = (datapoint << shift);
-                value = value + shifted_datapoint;
-                shift = shift + 8;
+            if (message_operators[i].bit_mode) {         
+                // printf("Skipping bit mode operator...\n");                
+                // uint8_t payload_le[msg->data_length_code];
+                uint64_t value_le = 0;
+                
+                for (unsigned int j = 0 ; j < data_length ; j++) {
+                    uint64_t shifted_value = ((uint64_t) msg->data[j] << (8 * j));
+                    // printf("Payload[%d]: %llu\n", j, shifted_value);
+                    // payload_le[j] = msg->data[data_length - j - 1];
+                    value_le = value_le + shifted_value;
+                    // printf("New value %llu\n", value_le);
+                }
+                // printf("%s: %llx\n", message_operators[i].name, value_le); 
+                
+                int num_bits = (int) log2(value_le) + 1;
+                value_le = value_le >> (message_operators[i].offset - num_bits);
+                int mask = (1 << message_operators[i].length) - 1;
+                int masked_value = value_le & mask;
+                float result = (masked_value * mult_factor) + add_factor;
+                printf("%s: %f\n", message_operators[i].name, result);
+
             }
-            value = value * mult_factor;
-            value = value + add_factor;
-            printf("%s: %d\n", message_operators[i].name, value);
+            else {
+                int payload_start = message_operators[i].offset;
+                int payload_end = payload_start + message_operators[i].length;
+    
+                int value = 0;
+                uint8_t shift = 0;
+                for (int j = payload_start; j < payload_end; j++) {
+                    uint8_t datapoint = msg->data[j];
+                    unsigned int shifted_datapoint = (datapoint << shift);
+                    value = value + shifted_datapoint;
+                    shift = shift + 8;
+                }
+                value = value * mult_factor;
+                value = value + add_factor;
+                printf("%s: %d\n", message_operators[i].name, value);
+            }
         }
     }
-    // have a list of message identifiers that you need to check
-
-
-    // if (!msg->rtr) {
-    //     printf("%d\n", msg_id);
-    //     for (int i = 0; i < msg->data_length_code; i++) {
-    //         printf("[%x]", msg->data[i]);
-    //     }
-    //     printf("\n");
-    // }
 }
 
 void app_main(void)
 {
+    uint64_t thign = 0xFF;
+    printf("%llu\n", thign << 56);
+
     twai_handle_t twai_bus_0;
-    // twai_handle_t twai_bus_1;
 
     // Initialize configuration structures using macro initializers
-    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_0, GPIO_NUM_1, TWAI_MODE_NORMAL);
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
-    twai_filter_config_t f_config = {.acceptance_code = 0, .acceptance_mask = 0xFFFFFFFF, .single_filter = true};
+    twai_general_config_t g_config_0 = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_10, GPIO_NUM_11, TWAI_MODE_LISTEN_ONLY);
+    twai_timing_config_t t_config_0 = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config_0 = {.acceptance_code = 0, .acceptance_mask =0xFFFFFFFF, .single_filter = true};
 
     // Install driver for TWAI bus 0
-    g_config.controller_id = 0;
-    if (twai_driver_install_v2(&g_config, &t_config, &f_config, &twai_bus_0) == ESP_OK) {
-        printf("TWAI Driver installed\n");
+    g_config_0.controller_id = 0;
+    if (twai_driver_install_v2(&g_config_0, &t_config_0, &f_config_0, &twai_bus_0) == ESP_OK) {
+        printf("TWAI [0] Driver installed\n");
     } else {
-        printf("Failed to install TWAI driver\n");
+        printf("Failed to install TWAI [0] driver\n");
         return;
     }
 
     // Start TWAI driver
     if (twai_start_v2(twai_bus_0) == ESP_OK) {
-        printf("TWAI Driver started\n");
+        printf("TWAI [0] Driver started\n");
     } else {
-        printf("Failed to start TWAI driver\n");
+        printf("Failed to start TWAI [0] driver\n");
         return;
     }
 
     while(true) {
         twai_message_t msg;
         if (twai_receive_v2(twai_bus_0, &msg, pdMS_TO_TICKS(5000)) == ESP_OK) {
+            // printf("Received msg\n");
             process_message(&msg);
         } else {
-            printf("Timeout or other error receiving message\n");
+            // printf("Timeout or other error receiving message\n");
             continue;
         }
     }
